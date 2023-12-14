9e37be68b4e4a3537a666d946e55eda50fb666b1
viktoria <viktoria_19_1@mail.ru>
2023-12-14 15:17:52 +0300

    modal

--- client/src/components/ui/NavBar.tsx
+++ client/src/components/ui/NavBar.tsx
@@ -12,16 +12,16 @@ export default function BasicExample(): React.JSX.Element {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">О нас</Nav.Link>
-            <NavDropdown title="Регионы" id="basic-nav-dropdown">
+            <Nav.Link href="#home" className='text-white'>О нас</Nav.Link>
+            <NavDropdown title="Регионы">
               <NavDropdown.Item href="#action/3.1">Дагестан</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Чечня</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Ингушетия</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.4">Кбр</NavDropdown.Item>
               <NavDropdown.Divider />
             </NavDropdown>
-            <Nav.Link href="#link">Войти</Nav.Link>
-            <Nav.Link href="#registr">Зарегистрироваться</Nav.Link>
+            <Nav.Link href="#link" className='text-white'>Войти</Nav.Link>
+            <Nav.Link href="#registr" className='text-white'>Зарегистрироваться</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
