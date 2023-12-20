99f416730b0bf1480cedca5af7baeef888d1e66a
viktoria <viktoria_19_1@mail.ru>
2023-12-20 11:36:30 +0300

    basket

--- client/src/components/pages/TourPage.tsx
+++ client/src/components/pages/TourPage.tsx
@@ -56,6 +56,10 @@ export default function ToursPage(): JSX.Element {
   const handleMoreClick = (id) => {
     window.location.href = `/more/${id}`;
   };
+
+  const handleBasketClick = () => {
+    window.location.href = `/basket`;
+  };
   console.log(selector, 'sdhkjgfdhjkfbxghjhfdbhjgfbjgf');
 
   return (
@@ -64,7 +68,7 @@ export default function ToursPage(): JSX.Element {
         <>
           {/* {console.log(tour, '============')} */}
 
-          <Card direction={{ base: 'column', sm: 'row' }} overflow="hidden" variant="outline">
+          <Card direction={{ base: 'column', sm: 'row' }} overflow="hidden" variant="outline" style={{marginTop: '20px'}}>
             <Carousel style={{ width: '600px', height: '300px' }}>
               <Carousel.Item style={{ width: '600px', height: '380px' }}>
                 <img src={tour.PhotoTour.img1} alt="12" />
